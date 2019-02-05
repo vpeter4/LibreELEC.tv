7a90b57219cc0bacf8d174725ae46e973d42a17b
Peter Vicman <peter.vicman@gmail.com>
2019-02-05 16:46:11 +0100

    lamp: updates

--- packages/addons/service/lamp/php/package.mk
+++ packages/addons/service/lamp/php/package.mk
@@ -32,16 +32,19 @@ PKG_TOOLCHAIN="autotools"
 #PKG_BUILD_FLAGS="-gold"
 #PKG_BUILD_FLAGS="+pic -gold"
 
-post_unpack() {
+dummy_post_unpack() {
   PHP_BUILD_DIR=$(get_build_dir php)
   echo "downloading pear..."
-  if [ ! -f "$PHP_BUILD_DIR/../go-pear.phar" ]; then
-    wget -O $PHP_BUILD_DIR/../go-pear.phar http://pear.php.net/go-pear.phar
-  fi
-  cp $PHP_BUILD_DIR/../go-pear.phar $PHP_BUILD_DIR/pear/go-pear.phar
+  wget -O ${SOURCES}/go-pear.phar https://github.com/pear/pearweb_phars/blob/master/go-pear.phar?raw=true
+  cp ${SOURCES}/go-pear.phar $PHP_BUILD_DIR/pear/go-pear.phar
 
   # libtool fix
   rm $PKG_BUILD/aclocal.m4
+}
+
+post_unpack() {
+  # libtool fix
+  rm -f $PKG_BUILD/aclocal.m4
 }
 
 configure_target() {
